2e72ee00dc66f6113bed66c356c79bfd663a8f2d
lrst6963 <59269350+lrst6963@users.noreply.github.com>
2022-04-17 22:04:52 +0800

    up

--- config/api.config.js
+++ config/api.config.js
@@ -25,10 +25,5 @@ module.exports = {
   // The scope we require are listed here, in most cases you would not need to change this as well.
   scope: 'user.read files.read.all offline_access',
 
-  // Cache-Control header, check Vercel documentation for more details. The default settings imply:
-  // - max-age=0: no cache for your browser
-  // - s-maxage=0: cache is fresh for 60 seconds on the edge, after which it becomes stale
-  // - stale-while-revalidate: allow serving stale content while revalidating on the edge
-  // https://vercel.com/docs/concepts/edge-network/caching
   cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
 }
